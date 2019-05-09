9d622039ca7488bea5c1c99567088145004eb0b6
Hauke Schulz <hauke27@gmail.com>
2019-05-09 20:47:54 +0200

    Added a padding class and unit tests

---
+++ Tests/Unit/PaddingTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace hschulz\imgix\Tests\Unit;
+
+use \hschulz\imgix\Padding;
+use \PHPUnit\Framework\TestCase;
+
+/**
+ * Description of PaddingTest
+ */
+final class PaddingTest extends TestCase
+{
+    public function testEmptyObjectReturnsNothing(): void
+    {
+        $subject = new Padding();
+
+        $this->assertEmpty((string) $subject);
+        $this->assertEmpty($subject->getQueryString());
+    }
+
+    public function testCanSetParams(): void
+    {
+        $subject = new Padding();
+
+        $params = [Padding::PARAM_NAME => 12];
+
+        $subject->setParams($params);
+
+        $this->assertArrayHasKey(Padding::PARAM_NAME, $subject->getParams());
+        $this->assertEquals($params, $subject->getParams());
+    }
+
+    public function testCanBeSet(): void
+    {
+        $subject = new Padding();
+
+        $subject->set(35);
+
+        $this->assertEquals(35, $subject->get());
+        $this->assertEquals(Padding::PARAM_NAME . '=35', (string) $subject);
+    }
+
+    public function testCanBeUnset(): void
+    {
+        $subject = new Padding();
+
+        $subject->set(3523);
+        $subject->unset();
+
+        $this->assertEmpty((string) $subject);
+    }
+}
---
+++ src/Padding.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace hschulz\imgix;
+
+use \hschulz\imgix\QueryEmitter;
+
+/**
+ * Add padding to your image.
+ */
+class Padding extends QueryEmitter
+{
+    /**
+     * Name of the padding parameter.
+     * @var string
+     */
+    const PARAM_NAME = 'padding';
+
+    /**
+     * Returns the set padding value or the default value.
+     *
+     * @return int The padding value or 0.
+     */
+    public function get(): int
+    {
+        return $this->params[self::PARAM_NAME] ?? 0;
+    }
+
+    /**
+     * Pads the image by the number of pixels specified.
+     * Must be a positive integer.
+     *
+     * If the image is requested without a specified width and height,
+     * padding will be added to the dimensions of the image. If the image is
+     * cropped or resized, the image (including the padding) will be sized to
+     * fit within the requested dimensions.
+     *
+     * If bg is set, the padded area will be filled in with its value;
+     * otherwise it will be transparent if the output format allows
+     * transparency, and white if not.
+     *
+     * @param int $padding
+     * @return void
+     */
+    public function set(int $padding): void
+    {
+        $this->params[self::PARAM_NAME] = $padding;
+    }
+
+    /**
+     * Unsets the padding value entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function unset(): void
+    {
+        unset($this->params[self::PARAM_NAME]);
+    }
+}
