ab86c3c9a33e2775a0b3ecdd5e1b209937dd632c
Hauke Schulz <hauke27@gmail.com>
2019-05-10 21:06:41 +0200

    Added color palette class and unit test

---
+++ Tests/Unit/ColorPaletteTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace hschulz\imgix\Tests\Unit;
+
+use \hschulz\imgix\ColorPalette;
+use \PHPUnit\Framework\TestCase;
+
+final class ColorPaletteTest extends TestCase
+{
+    public function testEmptyObjectReturnsNothing(): void
+    {
+        $subject = new ColorPalette();
+
+        $this->assertEmpty((string) $subject);
+        $this->assertEmpty($subject->getQueryString());
+    }
+
+    public function testCreateWithAllFeatures(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setColorCount(5);
+        $subject->setCssPrefix('test');
+        $subject->setPaletteOutputCss();
+
+        $expected = ColorPalette::PARAM_PALETTE_COLOR_COUNT . '=5&'
+                  . ColorPalette::PARAM_CSS_PREFIX . '=test&'
+                  . ColorPalette::PARAM_COLOR_PALETTE_EXTRATCTION . '=' . ColorPalette::VALUE_PALETTE_CSS;
+
+        $this->assertEquals($expected, (string) $subject);
+    }
+
+    public function testCanSetParams(): void
+    {
+        $subject = new ColorPalette();
+
+        $params = [ColorPalette::PARAM_PALETTE_COLOR_COUNT => 10];
+
+        $subject->setParams($params);
+
+        $this->assertArrayHasKey(ColorPalette::PARAM_PALETTE_COLOR_COUNT, $subject->getParams());
+        $this->assertEquals($params, $subject->getParams());
+    }
+
+    public function testCanSetCssPrefix(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setCssPrefix('unit');
+
+        $this->assertEquals('unit', $subject->getCssPrefix());
+        $this->assertEquals(ColorPalette::PARAM_CSS_PREFIX . '=unit', (string) $subject);
+    }
+
+    public function testCanUnsetCssPrefix(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setCssPrefix('unit');
+        $subject->unsetCssPrefix();
+
+        $this->assertFalse($subject->isPaletteOutputCss());
+        $this->assertFalse($subject->isPaletteOutputJson());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanSetPaletteOutputCss(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setPaletteOutputCss();
+
+        $this->assertTrue($subject->isPaletteOutputCss());
+        $this->assertEquals(ColorPalette::VALUE_PALETTE_CSS, $subject->getPaletteOutput());
+        $this->assertEquals(ColorPalette::PARAM_COLOR_PALETTE_EXTRATCTION . '=' . ColorPalette::VALUE_PALETTE_CSS, (string) $subject);
+    }
+
+    public function testCanSetPaletteOutputJson(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setPaletteOutputJson();
+
+        $this->assertTrue($subject->isPaletteOutputJson());
+        $this->assertEquals(ColorPalette::VALUE_PALETTE_JSON, $subject->getPaletteOutput());
+        $this->assertEquals(ColorPalette::PARAM_COLOR_PALETTE_EXTRATCTION . '=' . ColorPalette::VALUE_PALETTE_JSON, (string) $subject);
+    }
+
+    public function testCanUnsetPaletteOutput(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setPaletteOutputJson();
+        $subject->unsetPaletteOutput();
+
+        $this->assertFalse($subject->isPaletteOutputCss());
+        $this->assertFalse($subject->isPaletteOutputJson());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanSetColorCount(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setColorCount(12);
+
+        $this->assertEquals(12, $subject->getColorCount());
+        $this->assertEquals(ColorPalette::PARAM_PALETTE_COLOR_COUNT . '=12', (string) $subject);
+    }
+
+    public function testCanUnsetColorCount(): void
+    {
+        $subject = new ColorPalette();
+
+        $subject->setColorCount(16);
+        $subject->unsetColorCount();
+
+        $this->assertEmpty((string) $subject);
+    }
+}
---
+++ src/ColorPalette.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace hschulz\imgix;
+
+use \hschulz\imgix\QueryEmitter;
+
+/**
+ * Extracts a color palette from your image, which you can use to dynamically
+ * define other design elements in your layout.
+ *
+ * Palettes can be returned as CSS or JSON, and you can specify the number
+ * of colors in the palette as well as a class name prefix for CSS output.
+ */
+class ColorPalette extends QueryEmitter
+{
+    /**
+     * @var string
+     */
+    const PARAM_CSS_PREFIX = 'prefix';
+
+    /**
+     * @var string
+     */
+    const PARAM_COLOR_PALETTE_EXTRATCTION = 'palette';
+
+    /**
+     * @var string
+     */
+    const VALUE_PALETTE_CSS = 'css';
+
+    /**
+     * @var string
+     */
+    const VALUE_PALETTE_JSON = 'json';
+
+    /**
+     * @var string
+     */
+    const PARAM_PALETTE_COLOR_COUNT = 'colors';
+
+    /**
+     *
+     * @return string
+     */
+    public function getCssPrefix(): string
+    {
+        return $this->params[self::PARAM_CSS_PREFIX] ?? '';
+    }
+
+    /**
+     *
+     * @param string $prefix
+     * @return void
+     */
+    public function setCssPrefix(string $prefix): void
+    {
+        $this->params[self::PARAM_CSS_PREFIX] = $prefix;
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function unsetCssPrefix(): void
+    {
+        unset($this->params[self::PARAM_CSS_PREFIX]);
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getPaletteOutput(): string
+    {
+        return $this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION] ?? '';
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function setPaletteOutputCss(): void
+    {
+        $this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION] = self::VALUE_PALETTE_CSS;
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function setPaletteOutputJson(): void
+    {
+        $this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION] = self::VALUE_PALETTE_JSON;
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function unsetPaletteOutput(): void
+    {
+        unset($this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION]);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isPaletteOutputCss(): bool
+    {
+        return ($this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION] ?? '') === self::VALUE_PALETTE_CSS;
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isPaletteOutputJson(): bool
+    {
+        return ($this->params[self::PARAM_COLOR_PALETTE_EXTRATCTION] ?? '') === self::VALUE_PALETTE_JSON;
+    }
+
+    /**
+     *
+     * @return int
+     */
+    public function getColorCount(): int
+    {
+        return $this->params[self::PARAM_PALETTE_COLOR_COUNT] ?? 6;
+    }
+
+    /**
+     *
+     * @param int $count
+     * @return void
+     */
+    public function setColorCount(int $count): void
+    {
+        $this->params[self::PARAM_PALETTE_COLOR_COUNT] = $count;
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function unsetColorCount(): void
+    {
+        unset($this->params[self::PARAM_PALETTE_COLOR_COUNT]);
+    }
+}
