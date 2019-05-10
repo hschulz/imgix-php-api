86eb454faa5986a03a5005f5e3aa815b934e35ba
Hauke Schulz <hauke27@gmail.com>
2019-05-10 19:07:28 +0200

    Added crop mode class and unit test

---
+++ Tests/Unit/CropModeTest.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace hschulz\imgix\Tests\Unit;
+
+use \hschulz\imgix\CropMode;
+use \PHPUnit\Framework\TestCase;
+
+/**
+ * Description of CropModeTest
+ */
+final class CropModeTest extends TestCase
+{
+    public function testEmptyObjectReturnsNothing(): void
+    {
+        $subject = new CropMode();
+
+        $this->assertEmpty((string) $subject);
+        $this->assertEmpty($subject->getQueryString());
+    }
+
+    public function testCreateWithAllFeatures(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableBottom();
+        $subject->enableEdges();
+        $subject->enableEntropy();
+        $subject->enableFaces();
+        $subject->enableFocalpoint();
+        $subject->enableLeft();
+        $subject->enableRight();
+        $subject->enableTop();
+
+        $expected = CropMode::PARAM_NAME . '='
+                  . CropMode::VALUE_BOTTOM . ','
+                  . CropMode::VALUE_EDGES . ','
+                  . CropMode::VALUE_ENTROPY . ','
+                  . CropMode::VALUE_FACES . ','
+                  . CropMode::VALUE_FOCALPOINT . ','
+                  . CropMode::VALUE_LEFT . ','
+                  . CropMode::VALUE_RIGHT . ','
+                  . CropMode::VALUE_TOP;
+
+        $this->assertEquals($expected, (string) $subject);
+    }
+
+    public function testCanEnableBottom(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableBottom();
+
+        $this->assertTrue($subject->isBottomEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_BOTTOM, (string) $subject);
+    }
+
+    public function testCanDisableBottom(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableBottom();
+        $subject->disableBottom();
+
+        $this->assertFalse($subject->isBottomEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableEdges(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableEdges();
+
+        $this->assertTrue($subject->isEdgesEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_EDGES, (string) $subject);
+    }
+
+    public function testCanDisableEdges(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableEdges();
+        $subject->disableEdges();
+
+        $this->assertFalse($subject->isEdgesEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableEntropy(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableEntropy();
+
+        $this->assertTrue($subject->isEntropyEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_ENTROPY, (string) $subject);
+    }
+
+    public function testCanDisableEntropy(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableEntropy();
+        $subject->disableEntropy();
+
+        $this->assertFalse($subject->isEntropyEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableFaces(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableFaces();
+
+        $this->assertTrue($subject->isFacesEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_FACES, (string) $subject);
+    }
+
+    public function testCanDisableFaces(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableFaces();
+        $subject->disableFaces();
+
+        $this->assertFalse($subject->isFacesEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableFocalpoint(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableFocalpoint();
+
+        $this->assertTrue($subject->isFocalpointEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_FOCALPOINT, (string) $subject);
+    }
+
+    public function testCanDisableFocalpoint(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableFocalpoint();
+        $subject->disableFocalpoint();
+
+        $this->assertFalse($subject->isFocalpointEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableLeft(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableLeft();
+
+        $this->assertTrue($subject->isLeftEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_LEFT, (string) $subject);
+    }
+
+    public function testCanDisableLeft(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableLeft();
+        $subject->disableLeft();
+
+        $this->assertFalse($subject->isLeftEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableRight(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableRight();
+
+        $this->assertTrue($subject->isRightEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_RIGHT, (string) $subject);
+    }
+
+    public function testCanDisableRight(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableRight();
+        $subject->disableRight();
+
+        $this->assertFalse($subject->isRightEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableTop(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableTop();
+
+        $this->assertTrue($subject->isTopEnabled());
+        $this->assertEquals(CropMode::PARAM_NAME . '=' . CropMode::VALUE_TOP, (string) $subject);
+    }
+
+    public function testCanDisableTop(): void
+    {
+        $subject = new CropMode();
+
+        $subject->enableTop();
+        $subject->disableTop();
+
+        $this->assertFalse($subject->isTopEnabled());
+        $this->assertEmpty((string) $subject);
+    }
+}
---
+++ src/CropMode.php
@@ -0,0 +1,334 @@
+<?php
+
+namespace hschulz\imgix;
+
+use \hschulz\imgix\QueryEmitter;
+
+/**
+ * Crop mode controls how the image is aligned when fit=crop is set.
+ * The w and h parameters should also be set, so that the crop behavior is
+ * defined within specific image dimensions.
+ */
+class CropMode extends QueryEmitter
+{
+    /**
+     * @var string
+     */
+    const PARAM_NAME = 'crop';
+
+    /**
+     * @var string
+     */
+    const VALUE_TOP = 'top';
+
+    /**
+     * @var string
+     */
+    const VALUE_BOTTOM = 'bottom';
+
+    /**
+     * @var string
+     */
+    const VALUE_LEFT = 'left';
+
+    /**
+     * @var string
+     */
+    const VALUE_RIGHT = 'right';
+
+    /**
+     * @var string
+     */
+    const VALUE_FACES = 'faces';
+
+    /**
+     * @var string
+     */
+    const VALUE_FOCALPOINT = 'focalpoint';
+
+    /**
+     * @var string
+     */
+    const VALUE_EDGES = 'edges';
+
+    /**
+     * @var string
+     */
+    const VALUE_ENTROPY = 'entropy';
+
+    /**
+     * Returns the query string containing all enabled features.
+     *
+     * @return string
+     */
+    public function getQueryString(): string
+    {
+        /* When at least one feature is enabled the query string is created */
+        return count($this->params) > 0
+            ? self::PARAM_NAME . '=' . implode(',', $this->params) : '';
+    }
+
+    /**
+     *
+     * @param string $param
+     * @return void
+     */
+    protected function addParam(string $param): void
+    {
+        if (array_search($param, $this->params, true) === false) {
+            $this->params[] = $param;
+        }
+    }
+
+    /**
+     *
+     * @param string $param
+     * @return void
+     */
+    protected function removeParam(string $param): void
+    {
+        $pos = array_search($param, $this->params, true);
+
+        if ($pos !== false) {
+            unset($this->params[$pos]);
+        }
+    }
+
+    /**
+     * Crop from the top of the image, down.
+     *
+     * @return void
+     */
+    public function enableTop(): void
+    {
+        $this->addParam(self::VALUE_TOP);
+    }
+
+    /**
+     * Disable top cropping.
+     *
+     * @return void
+     */
+    public function disableTop(): void
+    {
+        $this->removeParam(self::VALUE_TOP);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isTopEnabled(): bool
+    {
+        return array_search(self::VALUE_TOP, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * Crop from the bottom of the image, up.
+     *
+     * @return void
+     */
+    public function enableBottom(): void
+    {
+        $this->addParam(self::VALUE_BOTTOM);
+    }
+
+    /**
+     * Disable bottom cropping.
+     *
+     * @return void
+     */
+    public function disableBottom(): void
+    {
+        $this->removeParam(self::VALUE_BOTTOM);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isBottomEnabled(): bool
+    {
+        return array_search(self::VALUE_BOTTOM, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * Crop from the left of the image, right.
+     *
+     * @return void
+     */
+    public function enableLeft(): void
+    {
+        $this->addParam(self::VALUE_LEFT);
+    }
+
+    /**
+     * Disable left cropping.
+     *
+     * @return void
+     */
+    public function disableLeft(): void
+    {
+        $this->removeParam(self::VALUE_LEFT);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isLeftEnabled(): bool
+    {
+        return array_search(self::VALUE_LEFT, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * Crop from the right of the image, left.
+     *
+     * @return void
+     */
+    public function enableRight(): void
+    {
+        $this->addParam(self::VALUE_RIGHT);
+    }
+
+    /**
+     * Disable right cropping.
+     *
+     * @return void
+     */
+    public function disableRight(): void
+    {
+        $this->removeParam(self::VALUE_RIGHT);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isRightEnabled(): bool
+    {
+        return array_search(self::VALUE_RIGHT, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * If faces are detected in the image, attempts to center the crop to them.
+     * Otherwise, the cropping alignment will default to centered
+     * if no additional values are provided.
+     *
+     * @return void
+     */
+    public function enableFaces(): void
+    {
+        $this->addParam(self::VALUE_FACES);
+    }
+
+    /**
+     * Disable face cropping.
+     *
+     * @return void
+     */
+    public function disableFaces(): void
+    {
+        $this->removeParam(self::VALUE_FACES);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isFacesEnabled(): bool
+    {
+        return array_search(self::VALUE_FACES, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * When set in combination with a relative horizontal (fp-x),
+     * vertical (fp-y), and/or zoom (fp-z) value, will center the image
+     * on those coordinates and crop from there.
+     *
+     * @return void
+     */
+    public function enableFocalpoint(): void
+    {
+        $this->addParam(self::VALUE_FOCALPOINT);
+    }
+
+    /**
+     * Disable focalpoint cropping.
+     *
+     * @return void
+     */
+    public function disableFocalpoint(): void
+    {
+        $this->removeParam(self::VALUE_FOCALPOINT);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isFocalpointEnabled(): bool
+    {
+        return array_search(self::VALUE_FOCALPOINT, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * The edges crop automatically finds and crops to an area of interest
+     * by performing edge detection, looking for objects within the image.
+     *
+     * @return void
+     */
+    public function enableEdges(): void
+    {
+        $this->addParam(self::VALUE_EDGES);
+    }
+
+    /**
+     * Disable edges cropping.
+     *
+     * @return void
+     */
+    public function disableEdges(): void
+    {
+        $this->removeParam(self::VALUE_EDGES);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isEdgesEnabled(): bool
+    {
+        return array_search(self::VALUE_EDGES, $this->params, true) === false ? false : true;
+    }
+
+    /**
+     * The entropy crop automatically finds and crops to an area of interest
+     * by looking for busy sections of the image.
+     *
+     * @return void
+     */
+    public function enableEntropy(): void
+    {
+        $this->addParam(self::VALUE_ENTROPY);
+    }
+
+    /**
+     * Disable entropy cropping.
+     *
+     * @return void
+     */
+    public function disableEntropy(): void
+    {
+        $this->removeParam(self::VALUE_ENTROPY);
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isEntropyEnabled(): bool
+    {
+        return array_search(self::VALUE_ENTROPY, $this->params, true) === false ? false : true;
+    }
+}
