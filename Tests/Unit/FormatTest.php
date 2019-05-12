4c179b7207eced5817c48c6b59c5bb3c0d760256
Hauke Schulz <hauke27@gmail.com>
2019-05-12 22:15:12 +0200

    Added format class and unit test

---
+++ Tests/Unit/FormatTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace hschulz\imgix\Tests\Unit;
+
+use \hschulz\imgix\Format;
+use \PHPUnit\Framework\TestCase;
+
+/**
+ * Description of FormatTest
+ */
+final class FormatTest extends TestCase
+{
+    public function testEmptyObjectReturnsNothing(): void
+    {
+        $subject = new Format();
+
+        $this->assertEmpty((string) $subject);
+        $this->assertEmpty($subject->getQueryString());
+    }
+
+    public function testCanSetMultipleParameters(): void
+    {
+        $subject = new Format();
+
+        $subject->setColorQuantization(16);
+        $subject->setDownload('unit.jpg');
+        $subject->setDpi(300);
+        $subject->enableLossless();
+        $subject->setQuality(100);
+
+        $expected = Format::PARAM_COLOR_QUANTIZATION . '=16&'
+                  . Format::PARAM_DOWNLOAD . '=unit.jpg&'
+                  . Format::PARAM_DPI . '=300&'
+                  . Format::PARAM_LOSSLESS . '=true&'
+                  . Format::PARAM_QUALITY . '=100';
+
+        $this->assertEquals($expected, (string) $subject);
+    }
+
+    public function testCanSetColorQuantization(): void
+    {
+        $subject = new Format();
+
+        $subject->setColorQuantization(32);
+
+        $this->assertEquals(32, $subject->getColorQuantization());
+        $this->assertEquals(Format::PARAM_COLOR_QUANTIZATION . '=32', (string) $subject);
+    }
+
+    public function testCanUnsetColorQuantization(): void
+    {
+        $subject = new Format();
+
+        $subject->setColorQuantization(8);
+        $subject->unsetColorQuantization();
+
+        $this->assertEquals(0, $subject->getColorQuantization());
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanSetDownload(): void
+    {
+        $subject = new Format();
+
+        $subject->setDownload('test.png');
+
+        $this->assertEquals('test.png', $subject->getDownload());
+        $this->assertEquals(Format::PARAM_DOWNLOAD . '=test.png', (string) $subject);
+    }
+
+    public function testCanUnsetDownload(): void
+    {
+        $subject = new Format();
+
+        $subject->setDownload('dl.gif');
+        $subject->unsetDownload();
+
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanSetDpi(): void
+    {
+        $subject = new Format();
+
+        $subject->setDpi(600);
+
+        $this->assertEquals(600, $subject->getDpi());
+        $this->assertEquals(Format::PARAM_DPI . '=600', (string) $subject);
+    }
+
+    public function testCanUnsetDpi(): void
+    {
+        $subject = new Format();
+
+        $subject->setDpi(120);
+        $subject->unsetDpi();
+
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanEnableLossless(): void
+    {
+        $subject = new Format();
+
+        $subject->enableLossless();
+
+        $this->assertTrue($subject->isLossless());
+        $this->assertEquals(Format::PARAM_LOSSLESS . '=true', (string) $subject);
+    }
+
+    public function testCanUnsetLossless(): void
+    {
+        $subject = new Format();
+
+        $subject->enableLossless();
+        $subject->disableLossless();
+
+        $this->assertEmpty((string) $subject);
+    }
+
+    public function testCanSetQuality(): void
+    {
+        $subject = new Format();
+
+        $subject->setQuality(30);
+
+        $this->assertEquals(30, $subject->getQuality());
+        $this->assertEquals(Format::PARAM_QUALITY . '=30', (string) $subject);
+    }
+
+    public function testCanUnsetQuality(): void
+    {
+        $subject = new Format();
+
+        $subject->setQuality(45);
+        $subject->unsetQuality();
+
+        $this->assertEmpty((string) $subject);
+    }
+}
---
+++ src/Format.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace hschulz\imgix;
+
+use \hschulz\imgix\QueryEmitter;
+
+/**
+ * This set of parameters gives you control over the output properties of your
+ * image across several aspects.
+ */
+class Format extends QueryEmitter
+{
+    /**
+     * Name of the color quantization parameter.
+     * @var string
+     */
+    const PARAM_COLOR_QUANTIZATION = 'colorquant';
+
+    /**
+     * Name of the dpi parameter.
+     * @var string
+     */
+    const PARAM_DPI = 'dpi';
+
+    /**
+     * Name of the download parameter.
+     * @var string
+     */
+    const PARAM_DOWNLOAD = 'dl';
+
+    /**
+     * Name of the lossless parameter.
+     * @var string
+     */
+    const PARAM_LOSSLESS = 'lossless';
+
+    /**
+     * Name of the quality parameter.
+     * @var string
+     */
+    const PARAM_QUALITY = 'q';
+
+    /**
+     * Returns the set color quantization value or the default value.
+     *
+     * @return int The color quantization value or '0' as the default value
+     */
+    public function getColorQuantization(): int
+    {
+        return $this->params[self::PARAM_COLOR_QUANTIZATION] ?? 0;
+    }
+
+    /**
+     * Limits the number of colors in a picture using color quantization,
+     * which reduces the distinct colors in an image while maintaining
+     * a visually-similar image.
+     *
+     * The effect is similar to that of posterization—decreasing the value
+     * of the colorquant parameter will reduce the gradation of tone and create
+     * abrupt changes from one tone to another.
+     *
+     * @param int $colors Valid values range between 2 – 256
+     * @return void
+     */
+    public function setColorQuantization(int $colors): void
+    {
+        $this->params[self::PARAM_COLOR_QUANTIZATION] = $colors;
+    }
+
+    /**
+     * Unsets the color quantization value entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function unsetColorQuantization(): void
+    {
+        unset($this->params[self::PARAM_COLOR_QUANTIZATION]);
+    }
+
+    /**
+     * Returns the set dpi value or the default value.
+     *
+     * @return int The set dpi value or '0' as the default value
+     */
+    public function getDpi(): int
+    {
+        return $this->params[self::PARAM_DPI] ?? 0;
+    }
+
+    /**
+     * Sets the DPI value in the Exif header. It does not alter the web display
+     * of the image.
+     *
+     * The DPI value affects the print size of an image. For instance,
+     * an 1800×1200-pixel image set to 300 DPI will print at a size of 6"×4".
+     *
+     * @param int $dpi
+     * @return void
+     */
+    public function setDpi(int $dpi): void
+    {
+        $this->params[self::PARAM_DPI] = $dpi;
+    }
+
+    /**
+     * Unsets the dpi entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function unsetDpi(): void
+    {
+        unset($this->params[self::PARAM_DPI]);
+    }
+
+    /**
+     * Returns the set download value or the default value.
+     *
+     * @return string The download value or '' as the default value
+     */
+    public function getDownload(): string
+    {
+        return $this->params[self::PARAM_DOWNLOAD] ?? '';
+    }
+
+    /**
+     * When used in a link, dl will force the browser to download the image instead
+     * of opening it in a new window. Set the value to your desired filename.
+     *
+     * @param string $filename The download filename
+     * @return void
+     */
+    public function setDownload(string $filename): void
+    {
+        $this->params[self::PARAM_DOWNLOAD] = $filename;
+    }
+
+    /**
+     * Unsets the download value entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function unsetDownload(): void
+    {
+        unset($this->params[self::PARAM_DOWNLOAD]);
+    }
+
+    /**
+     * The lossless parameter enables delivery of lossless images in formats
+     * that support lossless compression (JPEG XR and WEBP).
+     *
+     * @return void
+     */
+    public function enableLossless(): void
+    {
+        $this->params[self::PARAM_LOSSLESS] = 'true';
+    }
+
+    /**
+     * Unsets the lossless value entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function disableLossless(): void
+    {
+        unset($this->params[self::PARAM_LOSSLESS]);
+    }
+
+    /**
+     * Returns wether the lossless parameter is set.
+     *
+     * @return bool
+     */
+    public function isLossless(): bool
+    {
+        return $this->params[self::PARAM_LOSSLESS] ?? false;
+    }
+
+    /**
+     * Returns the set quality value or the default value.
+     *
+     * @return int The default value or '75' as the default value
+     */
+    public function getQuality(): int
+    {
+        return $this->params[self::PARAM_QUALITY] ?? 75;
+    }
+
+    /**
+     * Controls the output quality of lossy file formats (jpg, pjpg, webp, or jxr).
+     *
+     * Valid values are in the range 0 – 100 and the default is 75.
+     * Quality can often be set much lower than the default,
+     * especially when serving high-DPR images.
+     *
+     * @param int $quality Valid values are in the range 0 – 100
+     * @return void
+     */
+    public function setQuality(int $quality): void
+    {
+        $this->params[self::PARAM_QUALITY] = $quality;
+    }
+
+    /**
+     * Unsets the quality value entirely and therefore removes it
+     * from the query string.
+     *
+     * @return void
+     */
+    public function unsetQuality(): void
+    {
+        unset($this->params[self::PARAM_QUALITY]);
+    }
+}
